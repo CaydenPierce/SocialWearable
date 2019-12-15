f8faf630a960854c545a4cd91ac31fb025269589
Cayden <cpierc3@uwo.ca>
2019-12-14 21:56:31 -0500

    complete overhaul, new pose estimation library

--- ProcessorCode/bodylanguage.py
+++ ProcessorCode/bodylanguage.py
@@ -1,8 +1,6 @@
 import requests
 import cv2
 import time
-import poseinference
-from poseinference import model_names
 from time import sleep
 
 #globals for for fps calculation
@@ -10,7 +8,7 @@ counter = 0
 fpsTime = time.time()
 
 #setup video stream (MJPG)
-def setupCam(ip="192.168.1.2", port="8080"):
+def setupCam(ip="192.168.1.2", port="9000"):
 	cap = cv2.VideoCapture('http://{}:{}/?action=stream'.format(ip, port))
 	return cap
 
@@ -22,18 +20,20 @@ def killCam(cap):
 def getFrame(cap):
 	global counter, fpsTime
 	ret, frame = cap.read()
-	"""if ret:
+	if ret:
 		cv2.imshow('Frame', frame)
 		c = cv2.waitKey(10)
 	else:
 		return False
+	
+	#update fps display
 	counter += 1
 	if counter >= 10:
 		counter = 0
 		fps = 10 / (time.time() - fpsTime)
 		print("Current fps being read in is {} frames/seconds over last 10 frame. \r".format(fps))
 		fpsTime = time.time()
-	"""
+	
 	if ret == True:
 		return frame
 	else:
@@ -42,46 +42,17 @@ def getFrame(cap):
 def sendAction(actionName, ip="192.168.1.2", port="8081"): #use this to send the wearable/pi a message describing the action we just saw
 	resp = requests.post("http://{}:{}".format(ip, port), str(actionName))
 	print(resp)
+	return 1
 
 if __name__ == "__main__":
 	cap = setupCam()
-	import argparse
-	parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-	# Model structure
-	parser.add_argument('--arch', '-a', metavar='ARCH', default='hg',
-	choices=model_names,
-	help='model architecture: ' +
-	' | '.join(model_names) +
-	' (default: resnet18)')
-	parser.add_argument('-s', '--stacks', default=8, type=int, metavar='N',
-	help='Number of hourglasses to stack')
-	parser.add_argument('-b', '--blocks', default=1, type=int, metavar='N',
-	help='Number of residual modules at each location in the hourglass')
-	parser.add_argument('--num-classes', default=16, type=int, metavar='N',
-	help='Number of keypoints')
-	parser.add_argument('--mobile', default=False, type=bool, metavar='N',
-	help='use depthwise convolution in bottneck-block')
-	parser.add_argument('--checkpoint', required=True, type=str, metavar='N',
-	help='pre-trained model checkpoint')
-	parser.add_argument('--in_res', required=True, type=int, metavar='N',
-	help='input shape 128 or 256')
-	parser.add_argument('--image', default='data/sample.jpg', type=str, metavar='N',
-	help='input image')
-	parser.add_argument('--device', default='cuda', type=str, metavar='N',
-	help='device')
-
-	model, in_res_h, in_res_w = poseinference.loadModel(parser.parse_args())
 
 	timeCurr = time.time()
+	frames = 0
 	while True:
-		neck = False
+		frames += 1
 		cvframe = getFrame(cap)
-		image = poseinference.load_image(cvframe, in_res_h, in_res_w) #get frame, then load it in format to be sent through the model
-		actionDetected, actionName = poseinference.main(parser.parse_args(), model, in_res_h, in_res_w, image, cvframe)
-		#print("Time taken for one image: {}\r".format(time.time() - timeCurr))
-		print(actionDetected)
-		if actionDetected:
-			sendAction(actionName)
 		timeCurr = time.time()
 
 	killCam(cap)
+
--- ProcessorCode/voicerec/generatephraseTTS.py
+++ ProcessorCode/voicerec/generatephraseTTS.py
@@ -1,11 +1,13 @@
 from gtts import gTTS
 from playsound import playsound
 
-tts = gTTS(text="neck touch", lang='en')
+#this is a fast hack
+#TODO make it a module to be called whenever a new pose/person/whatever is added
+tts = gTTS(text="Cayden Pierce", lang='en')
 #tts.write_to_fp(audioAnswer)
-tts.save('./necktouch.mp3')
+tts.save('./CaydennPierce.mp3')
 
-tts = gTTS(text="head touch", lang='en')
+tts = gTTS(text="Abdallah Shami", lang='en')
 #tts.write_to_fp(audioAnswer)
-tts.save('./headtouch.mp3')
+tts.save('./AbdalahShami.mp3')
 #fill in text vars with the text you want to generate TTS
