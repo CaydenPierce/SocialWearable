61ec6c0fa1a0d4c0d6ac5bad12d44b021e0ae1a3
Cayden Pierce <cpierc3@uwo.ca>
2019-12-24 21:35:45 -0500

    return keypoings from getPose(). added BodyLanguage decoding class

--- ProcessorCode/bodylanguage.py
+++ ProcessorCode/bodylanguage.py
@@ -7,6 +7,7 @@ import time
 from time import sleep
 import torch
 import numpy as np
+import math
 
 #importing pose estimation library stuffs
 sys.path.insert(1, '../lightweight-human-pose-estimation.pytorch') #this points to the pose estimation library
@@ -17,9 +18,71 @@ from modules.pose import Pose, propagate_ids
 from val import normalize, pad_width
 from demo import infer_fast
 
-#globals for for fps calculation
+#globals for fps calculation
 counter = 0
 fpsTime = time.time()
+
+#extracts body language meaning from pose estimation keypoints
+"""for reference     kpt_names = ['nose', 'neck',
+                 'r_sho', 'r_elb', 'r_wri', 'l_sho', 'l_elb', 'l_wri',
+                 'r_hip', 'r_knee', 'r_ank', 'l_hip', 'l_knee', 'l_ank',
+                 'r_eye', 'l_eye',
+                 'r_ear', 'l_ear']
+"""
+
+class BL:
+    def __init__(keypoints, touchThresh=100):
+        self.keypoints = keypoints
+        self.touchThresh = touchThresh
+        kpt_names = ['nose', 'neck',
+                 'r_sho', 'r_elb', 'r_wri', 'l_sho', 'l_elb', 'l_wri',
+                 'r_hip', 'r_knee', 'r_ank', 'l_hip', 'l_knee', 'l_ank',
+                 'r_eye', 'l_eye',
+                 'r_ear', 'l_ear']
+        for i, name in kpt_names:
+            self.kpt_dict[name] = keypoints[i] 
+        self.result = pipeline()
+
+    def scaler(): #calculates a scaling factor based upon a very rough idea of how far away someone is
+        avgHeight = 170 #centimeter
+        avgTorsoProportion = 3/8 #torso is 3 / 8ths of height, https://www.researchgate.net/figure/Proportions-of-the-Human-Body-with-Respect-to-the-Height-of-the-Head_fig4_228867476
+        avgTorsoHeight = avgHeight * avgTorsoProportion #this doesn't actually mean anything in the real world until we map the camera output to real world numbers. Right now, just a number that will help us scale
+        
+        waistHeight = self.kpt_dict['r_hip'][1]
+        neckHeight = self.kpt_dict['neck'][1]
+        torsoHeight = neckHeight - waistHeight
+        self.scaler = torsoHeight / avgTorsoHeight
+
+    def neckTouch():
+        leftHandDist = distance(self.kpt_dict['l_wri'], self.kpt_dict['neck'])
+        rightHandDist = distance(self.kpt_dict['r_wri'], self.kpt_dict['neck'])
+        dist = min([leftHandDist, rightHandDist]) * self.scaler
+        if dist < self.touchThresh:
+            return True
+        else:
+            return False
+    
+    def headTouch():
+        leftHandDist = distance(self.kpt_dict['l_wri'], self.kpt_dict['head'])
+        rightHandDist = distance(self.kpt_dict['r_wri'], self.kpt_dict['head'])
+        dist = min([leftHandDist, rightHandDist]) * self.scaler
+        if dist < self.touchThresh:
+            return True
+        else:
+            return False
+
+    def distance(p1, p2):
+        x = p1[0] - p2[0]
+        y = p1[1] - p2[1]
+        dist = math.sqrt(x ** 2 + y ** 2)
+        return dist
+
+    def pipeline(): #pipe together body language movement decoders
+        neck = self.neckTouch()
+        head = self.headTouch()
+        result = {'neck' : neck, 'head' : head}
+        return result
+        
 
 #setup video stream (MJPG)
 def setupCam(ip="192.168.43.111", port="8080"):
@@ -45,7 +108,7 @@ def getFrame(cap):
 	if counter >= 10:
 		counter = 0
 		fps = 10 / (time.time() - fpsTime)
-		print("Current fps being read in is {} frames/seconds over last 10 frame. \r".format(fps))
+		print("Current fps being read in is {} frames/seconds over last 10 frames. \r".format(fps))
 		fpsTime = time.time()
 	
 	if ret == True:
@@ -56,49 +119,51 @@ def getFrame(cap):
 def sendAction(actionName, ip="192.168.1.2", port="8081"): #use this to send the wearable/pi a message describing the action we just saw
 	resp = requests.post("http://{}:{}".format(ip, port), str(actionName))
 	print(resp)
-	return 1
 
 def getPose(net, img, stride, upsample_ratio):
-		num_keypoints = Pose.num_kpts
-		previous_poses = []
-		orig_img = img.copy()
-		heatmaps, pafs, scale, pad = infer_fast(net, img, 256, stride, upsample_ratio, False)
+        num_keypoints = Pose.num_kpts
+        kpt_names = Pose.kpt_names
+        previous_poses = []
+        orig_img = img.copy()
+        heatmaps, pafs, scale, pad = infer_fast(net, img, 256, stride, upsample_ratio, False)
 
-		total_keypoints_num = 0
-		all_keypoints_by_type = []
-		for kpt_idx in range(num_keypoints):  # 19th for bg
-			total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
+        total_keypoints_num = 0
+        all_keypoints_by_type = []
+        for kpt_idx in range(num_keypoints):  # 19th for bg
+                total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
 
-		pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs, demo=True)
-		for kpt_id in range(all_keypoints.shape[0]):
-			all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
-			all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
-		current_poses = []
-		for n in range(len(pose_entries)):
-			if len(pose_entries[n]) == 0:
-				continue
-			pose_keypoints = np.ones((num_keypoints, 2), dtype=np.int32) * -1
-			for kpt_id in range(num_keypoints):
-				if pose_entries[n][kpt_id] != -1.0:  # keypoint was found
-					pose_keypoints[kpt_id, 0] = int(all_keypoints[int(pose_entries[n][kpt_id]), 0])
-					pose_keypoints[kpt_id, 1] = int(all_keypoints[int(pose_entries[n][kpt_id]), 1])
-			pose = Pose(pose_keypoints, pose_entries[n][18])
-			current_poses.append(pose)
-			pose.draw(img)
+        pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs, demo=True)
+        for kpt_id in range(all_keypoints.shape[0]):
+                all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
+                all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
+        current_poses = []
+        for n in range(len(pose_entries)):
+                if len(pose_entries[n]) == 0:
+                        continue
+                pose_keypoints = np.ones((num_keypoints, 2), dtype=np.int32) * -1
+                for kpt_id in range(num_keypoints):
+                        if pose_entries[n][kpt_id] != -1.0:  # keypoint was found
+                                pose_keypoints[kpt_id, 0] = int(all_keypoints[int(pose_entries[n][kpt_id]), 0])
+                                pose_keypoints[kpt_id, 1] = int(all_keypoints[int(pose_entries[n][kpt_id]), 1])
+                pose = Pose(pose_keypoints, pose_entries[n][18])
+                current_poses.append(pose)
+                pose.draw(img)
+                found_keypoints = pose.found_keypoints
 
-		img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
-		"""if track_ids == True:
-			propagate_ids(previous_poses, current_poses)
-			previous_poses = current_poses
-			for pose in current_poses:
-				cv2.rectangle(img, (pose.bbox[0], pose.bbox[1]),
-							  (pose.bbox[0] + pose.bbox[2], pose.bbox[1] + pose.bbox[3]), (0, 255, 0))
-				cv2.putText(img, 'id: {}'.format(pose.id), (pose.bbox[0], pose.bbox[1] - 16),
-							cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255))"""
-		cv2.imshow('Lightweight Human Pose Estimation Python Demo', img)
-		key = cv2.waitKey(33)
-		if key == 27:  # esc
-			return
+        img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
+        """if track_ids == True:
+                propagate_ids(previous_poses, current_poses)
+                previous_poses = current_poses
+                for pose in current_poses:
+                        cv2.rectangle(img, (pose.bbox[0], pose.bbox[1]),
+                                                  (pose.bbox[0] + pose.bbox[2], pose.bbox[1] + pose.bbox[3]), (0, 255, 0))
+                        cv2.putText(img, 'id: {}'.format(pose.id), (pose.bbox[0], pose.bbox[1] - 16),
+                                                cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255))"""
+        cv2.imshow('Lightweight Human Pose Estimation Python Demo', img)
+        key = cv2.waitKey(33)
+        if key == 27:  # esc
+                return
+        return found_keypoints
 
 if __name__ == "__main__":
 	net = PoseEstimationWithMobileNet()
@@ -113,7 +178,10 @@ if __name__ == "__main__":
 	while True:
 		frames += 1
 		cvframe = getFrame(cap)
-		getPose(net, cvframe, 8, 4)
+		pose = getPose(net, cvframe, 8, 4)
+                movements = BL(pose).results
+                print(pose)
+                print(movements)
 		timeCurr = time.time()
 
 	killCam(cap)
